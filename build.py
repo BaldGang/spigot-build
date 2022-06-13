69d3e287f6d05be9ea90390b0e9fb0c42147071a
xztdean <xztdean@gmail.com>
2022-06-13 02:49:57 -0700

    Fix typo

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ def build(version):
     os.system("java -Xmx6G -Xms6G -jar BuildTools.jar --rev %s --output-dir output --disable-java-check" % version)
 
 if __name__ == '__main__':
-    with open('config.yaml', 'r') as f:
+    with open('version.yml', 'r') as f:
         config = yaml.safe_load(f)
     for version in config[sys.argv[1]]:
         build(version)

d5b5614f1da9d4b5dc90ab3b6549f46934f94308
xztdean <xztdean@gmail.com>
2022-06-13 01:07:25 -0700

    Initial commit

---
+++ build.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import yaml
+
+def build(version):
+    os.system("java -Xmx6G -Xms6G -jar BuildTools.jar --rev %s --output-dir output --disable-java-check" % version)
+
+if __name__ == '__main__':
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+    for version in config[sys.argv[1]]:
+        build(version)

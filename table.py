43718d6fe27e3e20cfc44bad6b31f70df3b242bc
XZTDean <xztdean@gmail.com>
2025-12-15 00:40:28 -0800

    Update table generation script and bump version for GH action script

--- table.py
+++ table.py
@@ -1,11 +1,13 @@
 import os
 import re
+from typing import TextIO
 
-def get_version_map():
+def get_version_map() -> dict[int, dict[int, str]]:
     files = os.listdir()
     version_map = {}
+    pattern = re.compile(r'^spigot-1\.(\d{1,2})(?:\.(\d{1,2}))?.jar$')
     for file in files:
-        version = re.match(r'^spigot-1\.(\d{1,2})(?:\.(\d{1,2}))?.jar$', file)
+        version = pattern.match(file)
         if version is None:
             continue
         major = int(version.group(1))
@@ -19,12 +21,8 @@ def get_version_map():
         version_map[version] = dict(sorted(version_map[version].items()))
     return version_map
 
-def generate_table(versions, tag, file):
-    longest = 0
-    for version in versions:
-        length = len(versions[version])
-        if length > longest:
-            longest = length
+def generate_table(versions: dict[int, dict[int, str]], repo: str, tag: str, file: TextIO) -> None:
+    longest = max(len(versions[v]) for v in versions) if versions else 0
 
     file.write(f'| Version Family |')
     for _ in range(longest):
@@ -37,21 +35,22 @@ def generate_table(versions, tag, file):
     for version in versions:
         file.write(f'| 1.{version} |')
         for minor in versions[version]:
-            file.write(f' {generate_version_link(version, minor, tag)} |')
+            file.write(f' {generate_version_link(version, minor, repo, tag)} |')
         for _ in range(longest - len(versions[version])):
             file.write(' |')
         file.write('\n')
 
-def generate_version_link(major, minor, tag):
+def generate_version_link(major: int, minor: int, repo: str, tag: str) -> str:
     version = '1.' + str(major)
     if minor != 0:
         version += '.' + str(minor)
-    return f'[{version}](https://github.com/BaldGang/spigot-build/releases/download/{tag}/spigot-{version}.jar)'
+    return f'[{version}](https://github.com/{repo}/releases/download/{tag}/spigot-{version}.jar)'
 
 if __name__ == '__main__':
-    tag = os.sys.argv[1]
+    repo = os.sys.argv[1]
+    tag = os.sys.argv[2]
     versions = get_version_map()
-    output = os.sys.argv[2]
+    output = os.sys.argv[3]
     f = open(output, 'w')
-    generate_table(versions, tag, f)
+    generate_table(versions, repo, tag, f)
     f.close()
